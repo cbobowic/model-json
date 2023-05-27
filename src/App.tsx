480066a7124acc2d6f901337878217dbe328c42d
cbobowic <colden_bobowick@brown.edu>
2023-05-26 22:17:08 -0400

    deploy 1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Background from "./components/Background";
