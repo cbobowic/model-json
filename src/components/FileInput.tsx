d4167c8bbea928aebdb1ad1b6fec16fec58eb1e9
cbobowic <colden_bobowick@brown.edu>
2023-07-06 16:47:58 -0500

    feat: restructured fileloader. Significant ad hoc required.

--- src/components/FileInput.tsx
+++ src/components/FileInput.tsx
@@ -1,6 +1,4 @@
-import Input from "@mui/material/Input";
-import { useEffect, useRef, useState } from "react";
-import Button from "@mui/material/Button";
+import { useEffect, useState } from "react";
 import theme from "./ButtonTheme";
 import { ThemeProvider } from "@mui/material/styles";
 import * as tf from "@tensorflow/tfjs";
@@ -8,30 +6,38 @@ import Box from "@mui/material/Box";
 import CircularProgress from "@mui/material/CircularProgress";
 import Alert from "@mui/material/Alert";
 import Backdrop from "@mui/material/Backdrop";
-import Resizer from "react-image-file-resizer";
 import { model as m } from "../Constants";
+import FileUploader from "../utils/FileModelLoader";
 
 interface FileInputProps {
   onError: (error: Error) => void;
 }
 
-enum ModelState {
+export enum ModelState {
   FAKE,
   REAL,
   LOADING,
   IDLE,
-  MODEL_LOADING
+  MODEL_LOADING,
 }
 
-function FileInput({ onError}: FileInputProps) {
+function FileInput({ onError }: FileInputProps) {
   const [model, setModel] = useState<tf.GraphModel | null>(null);
   const [predictions, setPredictions] = useState<number[]>([]);
-  const [outputState, setOutputState] = useState<ModelState>(ModelState.MODEL_LOADING);
+  const [outputState, setOutputState] = useState<ModelState>(
+    ModelState.MODEL_LOADING
+  );
 
   useEffect(() => {
     async function loadModel() {
+      try {
       const newModel = await tf.loadGraphModel(m);
       setModel(newModel);
+      } catch {
+        onError(new Error("Error: The model could not be loaded! This is likely an issue on my end. Please try again later :("));
+        setOutputState(ModelState.MODEL_LOADING);
+        return;
+      }
     }
 
     if (model === null) {
@@ -43,88 +49,7 @@ function FileInput({ onError}: FileInputProps) {
     if (model !== null && outputState === ModelState.MODEL_LOADING) {
       setOutputState(ModelState.IDLE);
     }
-
-  }, [model, outputState]);
-
-  // Handles file input
-  const fileInputRef = useRef<HTMLInputElement>(null);
-
-  const handleButtonClick = () => {
-    if (fileInputRef.current) {
-      fileInputRef.current.click();
-    }
-  };
-
-  const resizeFile = (file: File) =>
-    new Promise((resolve) => {
-      Resizer.imageFileResizer(
-        file,
-        256,
-        256,
-        "JPEG",
-        100,
-        0,
-        (uri) => {
-          resolve(uri);
-        },
-        "file"
-      );
-    });
-
-  /**
-   * Used to handle when a file is uploaded to the file input.
-   *
-   * @param event file upload event
-   * @returns nothing
-   */
-  async function handleFileUpload(event: React.ChangeEvent<HTMLInputElement>) {
-    const files = event.target.files;
-    if (files !== null && files.length > 0) {
-      const rawFile = files[0];
-      if (
-        rawFile.type !== "image/jpeg" &&
-        rawFile.type !== "image/png" &&
-        rawFile.type !== "image/jpg"
-      ) {
-        onError(new Error("Error: Please select a *.jpeg, *.jpg, or *.png file!"));
-        setOutputState(ModelState.IDLE);
-        setPredictions([]);
-        return;
-      }
-      const file = (await resizeFile(rawFile)) as File;
-
-      setOutputState(ModelState.LOADING);
-
-      const img = new Image();
-      img.src = URL.createObjectURL(file);
-      img.onload = async () => {
-        try {
-          const tensor = tf.browser.fromPixels(img);
-          const resized = tf.image.resizeBilinear(tensor, [256, 256]).toFloat();
-          tensor.dispose();
-          const batched = resized.expandDims(0);
-          resized.dispose();
-          if (model === null) {
-            throw new Error("Error: The model could not be loaded! This is likely an issue on my end. Please try again later :(");
-          }
-          const predictions = model.predict(batched) as tf.Tensor;
-          const values = (await predictions.array()) as number[][];
-          setPredictions(values[0]);
-          if (values[0][0] > values[0][1]) {
-            setOutputState(ModelState.FAKE);
-          } else {
-          setOutputState(ModelState.REAL);
-          }
-        } catch (e) {
-          onError(e as Error);
-          setOutputState(ModelState.IDLE);
-          setPredictions([]);
-          return;
-        }
-      };
-    }
-  }
-
+  }, [model, outputState, onError]);
 
   // Handles color of the output alert
   function getColor() {
@@ -161,22 +86,14 @@ function FileInput({ onError}: FileInputProps) {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Button
-          aria-label="file input"
-          variant="outlined"
-          color="black"
-          onClick={handleButtonClick}
-          disabled = {outputState === ModelState.MODEL_LOADING}
-        >
-          { outputState === ModelState.MODEL_LOADING ? "Loading Model" : "Upload image"}
-        </Button>
+        <FileUploader
+          onError={onError}
+          model={model}
+          outputState={outputState}
+          setOutputState={setOutputState}
+          setPredictions={setPredictions}
+        />
       </ThemeProvider>
-      <Input
-        type="file"
-        inputRef={fileInputRef}
-        onChange={handleFileUpload}
-        style={{ display: "none" }}
-      />
       <Box
         sx={{
           display: "flex",
